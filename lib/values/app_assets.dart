fc9a0509b979d6b45b71b166c897ce1cb8fd7813
Chung Nguyen <zrmedia9@gmail.com>
2021-10-28 21:40:16 +0700

    Done add font and add  lib/values: colors, assets

---
+++ lib/values/app_assets.dart
@@ -0,0 +1,10 @@
+/// Define path of assets
+
+class AppAssets {
+  static const String imagePath = 'assets/images/';
+  static const String exchange = 'assets/images/exchange.png';
+  static const String heart = 'assets/images/heart.png';
+  static const String leftArrow = 'assets/images/left-arrow.png';
+  static const String rightArrow = 'assets/images/right-arrow.png';
+  static const String menu = 'assets/images/menu.png';
+}
