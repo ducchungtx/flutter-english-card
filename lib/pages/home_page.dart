0fec5b4d21f5f9cf728e0744936cc9e376f8b8c1
Chung Nguyen <zrmedia9@gmail.com>
2021-10-29 14:41:09 +0700

    Update code home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _curentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(viewportFraction: 0.5);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -30,11 +39,11 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: <Widget>[
             Container(
               height: size.height * 1 / 10,
-              padding: EdgeInsets.all(16),
               alignment: Alignment.centerLeft,
               child: Text(
                 'It is amazing how complete is the delusion that beauty is godness.',
@@ -45,15 +54,83 @@ class _HomePageState extends State<HomePage> {
             Container(
               height: size.height * 2 / 3,
               child: PageView.builder(
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _curentIndex = index;
+                    });
+                  },
                   itemCount: 5,
                   itemBuilder: (context, index) {
                     return Container(
-                        margin: const EdgeInsets.all(16),
-                        decoration: const BoxDecoration(
-                            color: AppColor.primaryColor,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular((24)))),
-                        child: Text('$index'));
+                      margin: const EdgeInsets.symmetric(horizontal: 16),
+                      decoration: const BoxDecoration(
+                          color: AppColor.primaryColor,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular((24)))),
+                      child: Column(
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            alignment: Alignment.centerRight,
+                            child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 10),
+                                child: Image.asset(AppAssets.heart)),
+                          ),
+                          RichText(
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            text: const TextSpan(
+                                text: 'B',
+                                style: TextStyle(
+                                    fontFamily: FontFamily.sen,
+                                    fontSize: 89,
+                                    fontWeight: FontWeight.bold,
+                                    shadows: [
+                                      BoxShadow(
+                                          color: Colors.black38,
+                                          offset: Offset(3, 6),
+                                          blurRadius: 6)
+                                    ]),
+                                children: [
+                                  TextSpan(
+                                      text: 'eautiful',
+                                      style: TextStyle(
+                                          fontFamily: FontFamily.sen,
+                                          fontSize: 56,
+                                          fontWeight: FontWeight.bold,
+                                          shadows: [
+                                            BoxShadow(
+                                                color: Colors.black38,
+                                                offset: Offset(3, 6),
+                                                blurRadius: 6)
+                                          ]))
+                                ]),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 10),
+                            child: Text(
+                              '"Think of all the beauty still left around you and be happy."',
+                              style: AppStyles.h4.copyWith(letterSpacing: 1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+            Container(
+              height: 12,
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(vertical: 12),
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return buildIndicator(index == _curentIndex, size);
                   }),
             )
           ],
@@ -64,6 +141,20 @@ class _HomePageState extends State<HomePage> {
         onPressed: () {},
         child: Image.asset(AppAssets.exchange),
       ),
+    );
+  }
+
+  Widget buildIndicator(bool isActive, Size size) {
+    return Container(
+      height: 8,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      width: isActive ? size.width * 1 / 5 : 24,
+      decoration: BoxDecoration(
+          color: isActive ? AppColor.lightBlue : AppColor.lightGrey,
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          boxShadow: const [
+            BoxShadow(color: Colors.black38, offset: Offset(2, 3))
+          ]),
     );
   }
 }
